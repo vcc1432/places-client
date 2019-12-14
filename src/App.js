4d50f868588695b75364b8f4af180eac0149f892
vcc1432 <valerie.gersen@gmail.com>
2019-12-14 15:40:03 +0100

    Setup project, user, navigation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return(
     <Router>
+      <MainNavigation/>
+      <main>
       <Switch>
         <Route path="/" exact>
           <Users />
@@ -15,6 +18,7 @@ const App = () => {
         </Route> 
         <Redirect to="/" />
       </Switch>
+      </main>
     </Router>
   );
 };
