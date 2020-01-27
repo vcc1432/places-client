743548cad22ce125c650a7fc07d985fa11b1b1b7
vcc1432 <valerie.gersen@gmail.com>
2020-01-27 23:31:41 +0100

    Added http hook

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,49 @@
+import { useState, useCallback, useRef, useEffect } from 'react';
+
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeHttpRequests = useRef([]);
+
+  const sendRequest = useCallback(async (URL, method = 'GET', body = null, headers= {}) => {
+    setIsLoading(true);
+    const httpAbortCtrl = new AbortController();
+    activeHttpRequests.current.push(httpAbortCtrl);
+
+    try {
+      const response = await fetch(url, {
+        method,
+        body,
+        headers,
+        signal: httpAbortCtrl.signal
+      })
+
+      const responseData = await response.json();
+      console.log(responseData)
+
+      if (!response.ok) {
+        throw new Error(responseData.message);
+      }
+
+      return responseData;
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsLoading(false);
+
+  }, []);
+
+  clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abort())
+    }; // cleanup function
+  }, [])
+
+  return { isLoading, error, sendRequest, clearError}
+};
